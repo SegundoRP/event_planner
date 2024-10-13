23f2a02501b06c8f5bb1d1f60311a62288d5b6a8
Segundo Rebaza <alvaro.rp1593@gmail.com>
2024-10-13 14:01:39 -0500

    style: improve styles of views

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,8 @@ module EventsHelper
   def event_duration(event)
     ((event.end_time - event.start_time) / 60).to_i
   end
+
+  def full_name(user)
+    "#{user.first_name} #{user.last_name}"
+  end
 end
